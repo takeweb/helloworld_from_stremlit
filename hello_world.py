2db210a5542d912b5a2bd29011a26891c72df5d3
Taketomo Ohishi <takeweb@mac.com>
2025-04-24 08:48:02 +0900

    initial commit

---
+++ hello_world.py
@@ -0,0 +1,13 @@
+import streamlit as st
+
+# st.title('Hello World')
+st.title('title string `<h1>`')
+st.header('大見出し `<h2>`')
+st.subheader(
+    body='中見出し`<h3>`',
+    anchor='title',
+    help='`<h3>`あるいは`###に相当するStreamlitのコマンド`',
+    divider=True
+)
+st.snow()
+st.balloons()
---
+++ main.py
@@ -0,0 +1,6 @@
+def main():
+    print("Hello from helloworld-from-stremlit!")
+
+
+if __name__ == "__main__":
+    main()
